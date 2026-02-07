29b595db683e3a1935132a5c5d7f7aacb2d10d18
PILIP.YOM <PILIP.YOM@pnc.local>
2026-02-07 10:19:50 +0700

    test

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Scroll reveal animation
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (top < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
